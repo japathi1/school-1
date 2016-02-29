4102187934645bb2fc48afa592c9a322a8ea2a15
priyranjansingh <singh.priyranjan@gmail.com>
2016-02-29 08:05:01 +0530

    fee labe

--- protected/modules/school/views/feeLabel/_search.php
+++ protected/modules/school/views/feeLabel/_search.php
@@ -1,65 +1,18 @@
-<?php
-/* @var $this FeeLabelController */
-/* @var $model FeeLabel */
-/* @var $form CActiveForm */
-?>
-
-<div class="wide form">
-
-<?php $form=$this->beginWidget('CActiveForm', array(
+<?php $form = $this->beginWidget('CActiveForm', array(
 	'action'=>Yii::app()->createUrl($this->route),
 	'method'=>'get',
+	'htmlOptions'=>array('class' => 'search-form')
 )); ?>
-
-	<div class="row">
-		<?php echo $form->label($model,'id'); ?>
-		<?php echo $form->textField($model,'id',array('size'=>36,'maxlength'=>36)); ?>
+<div class="box-body">
+	<div class="form-group">
+		<div class="col-xs-3">
+			<?php echo $form->label($model,'fee_label'); ?>
+			<?php echo $form->textField($model,'fee_label',array('size'=>60,'maxlength'=>255,'class' => 'form-control')); ?>
+		</div>
 	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'school_id'); ?>
-		<?php echo $form->textField($model,'school_id',array('size'=>36,'maxlength'=>36)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'fee_label'); ?>
-		<?php echo $form->textField($model,'fee_label',array('size'=>60,'maxlength'=>256)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'status'); ?>
-		<?php echo $form->textField($model,'status'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'deleted'); ?>
-		<?php echo $form->textField($model,'deleted'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'date_entered'); ?>
-		<?php echo $form->textField($model,'date_entered'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'date_modified'); ?>
-		<?php echo $form->textField($model,'date_modified'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'created_by'); ?>
-		<?php echo $form->textField($model,'created_by',array('size'=>36,'maxlength'=>36)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'modified_by'); ?>
-		<?php echo $form->textField($model,'modified_by',array('size'=>36,'maxlength'=>36)); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Search'); ?>
-	</div>
-
+</div>
+<div class="box-footer">
+	<?php echo CHtml::submitButton('Search',array("class" => 'btn btn-info search-button')); ?>
+	<a href="<?php echo base_url().'/school/feelabel/manage' ?>" class="btn btn-warning">Clear</a>
+</div>
 <?php $this->endWidget(); ?>
-
-</div><!-- search-form -->
